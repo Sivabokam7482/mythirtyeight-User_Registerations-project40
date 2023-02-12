5b293356f15635b3308337ddbcf69d02d24e87ca
Sivabokam7482 <sivabokam7482@gmail.com>
2023-02-12 15:26:33 +0530

    first commit

--- app1/models.py
+++ app1/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
-    profile_name=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_user=models.OneToOneField(User,on_delete=models.CASCADE)
     address=models.TextField()
     profile_pic=models.ImageField(upload_to='siva')
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from app1.forms import *
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def register(request):
+    UF=UserForm()
+    PF=ProfileForm()
+    d={'uf':UF,'pf':PF}
+
+    if request.method=='POST' and request.FILES:
+        UFD=UserForm(request.POST)
+        PFD=ProfileForm(request.POST,request.FILES)
+
+        if UFD.is_valid() and PFD.is_valid():
+            UFO=UFD.save(commit=False)
+            password=UFD.cleaned_data['password']
+            UFO.set_password(password)
+            UFO.save()
+
+            PFO=PFD.save(commit=False)
+            PFO.profile_user=UFO
+            PFO.save()
+            return HttpResponse('Registration Successfully')
+
+    return render(request,'Registration.html',d)
